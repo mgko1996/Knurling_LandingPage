40dc0f4add9692b70dfe7f4cbd4c475530b442a5
mgko1996 <mgko_424@dongguk.edu>
2021-08-05 18:08:01 +0900

    photo and requirement

--- knurling/settings.py
+++ knurling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pvf)%3yjlf+d+ptz7zzem@ltxrq3q_n3i_d)7z7cir-)t5h_((
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
